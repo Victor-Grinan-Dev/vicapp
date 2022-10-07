017c91ad0a321dd6a019cbc6dc26e5104bd8a5e4
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-10-07 09:36:18 +0300

    router creater

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Layout from './pages/Layout';
+
+import Home from './components/home/Home';
+import About from './components/about/About';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>  
+
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from '../components/Main';
+
+const Layout = () => {
+  return (
+    <div>
+        <div>header</div>
+        <Main/>
+        <div>footer</div>
+    </div>
+  )
+}
+
+export default Layout;
